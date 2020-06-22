341d428d119828ccd774a8a2bc71e6f00008df17
daska <d.askar92@gmail.com>
2020-06-22 15:17:03 -0400

    Added the counter class & the count on state & added CSS and JSX

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const { Component } = require("react");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+      <div className="navbar">Counter.js</div>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Counter;
